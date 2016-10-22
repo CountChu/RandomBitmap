2a74bb877c5cc645163fc4d592aa97f21a193c63
CountChu <visualge@gmail.com>
2016-10-22 22:40:43 +0800

    Add debug log. Refine display messages

--- Code/BinToImg.py
+++ Code/BinToImg.py
@@ -1,11 +1,20 @@
 import sys
 from PIL import Image
 import math
+import logging
 
 def main ():
+    global logger
+
+    loger_init (logging.INFO)    
+    #loger_init (logging.DEBUG)    
+
+    print ("")
+    print ("======== BinToImg ========")    
 
     if len (sys.argv) != 2:
         print "Usage: python BinToImage.py File.bin"
+
 
     bin_fn = sys.argv [1]
     png_fn = bin_fn + ".png"
@@ -13,39 +22,53 @@ def main ():
     bin_to_img (bin_fn, png_fn)
     sys.exit (0)    
 
+def loger_init (level):
+    global logger
+
+    ch = logging.StreamHandler()
+    ch.setFormatter (logging.Formatter("")) # Remove prefix.
+    #ch.terminator = "\n\n\n\n"         # No newline character
+
+    logger = logging.getLogger ('MyLogger')
+    logger.setLevel(level)    
+    logger.addHandler (ch)
+
 def bin_to_img (bin_fn, png_fn):
+    global logger    
 
     f = open (bin_fn, "rb")
     bytes = f.read()
-    print ("len data = %d" % len(bytes))
+    print ("The length of the file %s is %d in bytes" % (bin_fn, len(bytes)))
     img_area = 8 * len (bytes)
-    print ("img_area = %d" % img_area)
+    print ("Image Area is %d" % img_area)
     img_size = int (math.sqrt (img_area))
-    print ("img_size = %d" % img_size)
+    print ("Image Size is (%d, %d)" % (img_size, img_size))
 
     x = 0
     y = 0
     im = Image.new ("RGB", (img_size, img_size), "white")
     pixels = im.load ()
+    sys.stdout.write ("y = %d " % y)
     for byte in bytes:
         byte = ord (byte)
-        sys.stdout.write ("%02x " % byte)
-        print ("")
+        log = "%02x ---> " % byte
         for idx in range (0, 8):
             r = (byte >> idx) % 2
-            sys.stdout.write ("%d" % r)
+            log += ("%d" % r)
             if r == 1:
-                sys.stdout.write ("(%d, %d)" % (x, y))
+                log += ("(%d, %d)" % (x, y))
                 pixels [x, y] = (0, 0, 0)
             x += 1
             if x == img_size:
                 x = 0
                 y += 1
+                sys.stdout.write ("%d " % y)
             if y >= img_size:
                 break
+            log += " "
         if y >= img_size:
             break           
-        #print ("")
+        logger.debug (log)
     print ("")
 
     if x != 0:
@@ -57,5 +80,6 @@ def bin_to_img (bin_fn, png_fn):
 
     im.show ()
     im.save (png_fn)
+    print ("Generate the PNG file, %s" % png_fn)
 
 main()
--- Code/GenRandom.py
+++ Code/GenRandom.py
@@ -1,9 +1,16 @@
 
+import logging
 import sys
 import RandomGenerator
 
-
 def main():
+    global logger
+
+    loger_init (logging.INFO)
+
+    print ("")
+    print ("======== GenRandom ========")
+
     if len (sys.argv) != 2:
         print "Usage: python GenRandom.py File.bin"
         sys.exit(1)
@@ -14,27 +21,42 @@ def main():
     gen_random (out_fn, RandomGenerator.rg1)
     sys.exit(0)
 
+def loger_init (level):
+    global logger
+
+    ch = logging.StreamHandler()
+    ch.setFormatter (logging.Formatter("")) # Remove prefix.
+    #ch.terminator = "\n\n\n\n"         # No newline character
+
+    logger = logging.getLogger ('MyLogger')
+    logger.setLevel(level)    
+    logger.addHandler (ch)
+
 def gen_random (out_fn, random_generator):
+    global logger    
 
     f= open (out_fn, "wb")
+
     width = 512
     height = 512
+    print ("Image Size is (%d, %d)" % (width, height))    
+    
     idx = 0
     byte = 0
+    log = ""
     for y in range (0, width):
         for x in range (0, height):
             r = random_generator ()
-            sys.stdout.write ("%d" % r)
+            log += ("%d" % r)
             byte += r << idx
             idx += 1
             if idx == 8:
                 f.write (chr(byte))
-                print ("")
-                print ("%02x" % byte)
+                log += " ---> %02x\n" % byte
                 idx = 0
                 byte = 0
-        print ("")
+        logger.debug (log)
     f.close()
+    print ("Generate the file, %s" % out_fn)    
 
 main()
-
